91146cadd56ed657e44ce0ec42ddb9a8622071e6
SideRay <sidmar.88@hotmail.com>
2023-08-23 16:25:30 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,13 @@
+# executavel com cxfreeze
+# 1 - Gerar o executavel ( digitar no terminal cxfreeze app.py)
+# 2 - tranformar o executavel em um instalador
+
+
+
+
+from time import sleep 
+
+for number in range(30):
+    print(number)
+    sleep(1)
+
